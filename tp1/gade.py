2336ac73b39fe1eaa8f8cbf754598fe6611b9d3b
hg-heds <hugo.hedsts.u@gmail.com>
2023-05-20 09:18:10 -0300

    de_type

--- tp1/gade.py
+++ tp1/gade.py
@@ -29,20 +29,45 @@ def ga(pop_size = 100, dim = 2, max_gen = 400, lim = [-5.12,5.12]):
         population = temp_pop[index]
     return min(rastrigin(population))
 
-def de(pop_size = 100, dim = 2, max_gen = 400, lim = [-5.12,5.12]):
+def de(pop_size = 100, dim = 2, max_gen = 400, lim = [-5.12,5.12], de_type = 'rand-1'):
     xmin, xmax = lim
     population = lim[0] + np.random.rand(pop_size,dim) * (xmax-xmin)
+    f_pop = rastrigin(population)
     for _ in range(max_gen):
         for i in range(pop_size):
-            C = np.random.rand()+0.5
+            eta = np.random.rand()+0.5
             locked = np.random.randint(dim)
-            r1,r2,r3 = np.random.permutation(pop_size)[:3]
-            child = population[r1] + C*(population[r3]-population[r2])
+            ################################################################# rand-1
+            if de_type == 'rand-1':
+                r1,r2,r3 = np.random.permutation(pop_size)[:3]
+                child = population[r1] + eta*(population[r2]-population[r3])
+            ################################################################# best-1
+            elif de_type == 'best-1':
+                best = np.argmin(f_pop)
+                r2,r3 = np.random.permutation(pop_size)[:2]
+                child = population[best] + eta*(population[r2]-population[r3])
+            ################################################################# mean-1
+            elif de_type == 'mean-1':
+                r2,r3 = np.random.permutation(pop_size)[:2]
+                child = np.mean(population,axis=0) + eta*(population[r2]-population[r3])
+            ################################################################# mean-to-best-1
+            elif de_type == 'mean-to-best-1':
+                best = np.argmin(f_pop)
+                lambda_ = np.random.rand()
+                r2,r3 = np.random.permutation(pop_size)[:2]
+                child = population[i] + lambda_*(population[best]-population[i]) + eta*(population[r2]-population[r3])
+            ################################################################# rand-2
+            elif de_type == 'rand-2':
+                eta2 = np.random.rand()+0.5
+                r1,r2,r3,r4,r5 = np.random.permutation(pop_size)[:5]
+                child = population[r1] + eta*(population[r2]-population[r4]) + eta2*(population[r3]-population[r5])
+
             rand = np.random.rand(dim)<0.5
             rand[locked] = False
             child[rand] = population[i][rand]
             if rastrigin(child) <= rastrigin(population[i]):
                 population[i] = child
+                f_pop[i] = rastrigin(child)
     return min(rastrigin(population))
 
 def test():
